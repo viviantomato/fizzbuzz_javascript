f4df4772a1c554cae9c8f269f018f3c9e33f254d
viviantomato <vivianyw924@gmail.com>
2022-12-01 10:54:45 -0800

    fix test

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,9 +1,13 @@
 export const fizzBuzz = (num) => {
+  console.log(num);
   if (num % 15 === 0) {
     return 'FizzBuzz';
+  } else if (num % 3 === 0) {
+    return 'Fizz';
   } else if (num % 5 === 0) {
     return 'Buzz';
   } else {
+    console.log("Returning num:")
     return num;
   }
 };
